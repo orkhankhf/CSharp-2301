e05e431aa0329a82465854f60f39ec7d8ee57321
Orkhan Farajov <orkhankhf@gmail.com>
2023-04-29 17:56:23 +0400

    MSSQL start

---
+++ Aprel/28/LessonQueries.sql
@@ -0,0 +1,23 @@
+﻿use AdventureWorks2017;
+
+select * from AdventureWorks2017.Person.Address;
+
+--This is single line comment
+
+/*
+This
+is
+multi
+line
+comment
+*/
+
+use AdventureWorks2017; --Hansı database'i istifadə etsin aşağıdakı kodlar üçün onu bildirirsiniz.
+
+--Select: Table'dan məlumatları çəkən əmr
+--From: Haradan və ya hansı qaynaqdan məlumatları götürəcəyini bildirir.
+
+--Yazılış qaydası: select column_adları (və ya) * from table_adi
+select * from Person.Address; --Address table'ında olan bütün column'ları (sütunları) gətir.
+
+select AddressId, AddressLine1, City, PostalCode, ModifiedDate from Person.Address;
