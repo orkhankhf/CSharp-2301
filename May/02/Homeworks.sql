e350bb356f71e42381dbc4523d34c7c3772811f8
Orkhan Farajov <orkhankhf@gmail.com>
2023-05-03 00:03:00 +0400

    Sql basic queries

---
+++ May/02/Homeworks.sql
@@ -0,0 +1,83 @@
+﻿/*
+Praktiki tapsiriqlar 1:
+1. Production.Product table'ından ProductID'si 20-dən böyük olan məlumatların bütün column'larını gətir.
+2. Production.Product table'ından ProductID'si 20-dən kiçik olan məlumatların bütün column'larını gətir.
+3. Production.Product table'ından ListPrice'ı 200-dən böyük, 500-dən kiçik olan məlumatların ProductID, Name və ListPrice column'larını gətir.
+4. Production.Product table'ından SafetyStockLevel'i 900-dən böyük, 1500-dən kiçik olan və ProductID'si 10 olmayan məlumatların bütün column'larını gətir.
+5. Production.Product table'ından ProductNumber'ı 'BA-8327' olmayan, Color'u 'Black' olan ilk 50 dənə məlumatın bütün column'larını gətir.
+6. Production.Product table'ından ListPrice'ı 500-dən kiçik və ya bərabər olan məlumatların 50%'ni gətir.
+7. Production.Product table'ından ListPrice'ı 1001-dən böyük və ya bərabər olan məlumatların 25%'ni gətir.
+8. Production.Product table'ından Color'u 'Black' və ya 'Red' olan, Color'u 'Yellow' olmayan məlumatların bütün column'larını gətir.
+9. Production.Product table'ından ProductID'si 100-dən böyük, 500-dən kiçik olan və 300, 500 olmayan məlumatların ProductID column'unu gətir.
+10. Production.Product table'ından SafetyStockLevel'i 800 və ya 1000 olan amma Color'u 'Black' və ya 'Blue' olmayan məlumatların bütün column'larını gətir.
+11. Person.Address table'ından City, PostalCode və AddressLine1 column'larının dəyərlərini aralarında vergül olmaqla FullAddress column adı ilə çəkin.
+
+Praktiki tapsiriqlar 2:
+1. Person.PersonPhone table'ından PhoneNumber column'unda 15 rəqəmi olan məlumatların bütün column'larını gətir.
+2. Person.PersonPhone table'ından PhoneNumber'i 1 rəqəmilə başlayan məlumatların bütün column'larını gətir.
+3. Person.PersonPhone table'ından PhoneNumber'i 15 rəqəmilə bitən məlumatların bütün column'larını gətir.
+4. Person.PersonPhone table'ından PhoneNumber column'unda 82 rəqəmi olan məlumatların bütün column'larını gətir.
+5. Person.ContactType table'ından Name column'unda a hərfi olan məlumatların bütün column'larını gətir.
+6. Person.ContactType table'ından Name'i r hərfilə başlayan məlumatların bütün column'larını gətir.
+7. Person.ContactType table'ından Name'i s hərfilə bitən məlumatların bütün column'larını gətir.
+8. Person.Person table'ından FirstName'i a hərfilə bitən və o hərfilə başlayan məlumatların bütün column'larını gətir.
+9. Person.Person table'ından FirstName'i a hərfilə bitən və o hərfilə başlayan və LastName'ında 'ver' sözü olan məlumatların bütün column'larını gətir.
+10. Person.Person table'ından FirstName'i a hərfilə bitən və PersonType'ı ZT olmayan ilk 40 məlumatın bütün column'larını gətir.
+
+
+VACİB QEYD: Taskların heç birini edərkən qətiyyən copy-paste etməyin. Bütün sorğuları bir-bir özünüz yazın.
+
+Bütün taskları Adventure Works database'ində işləyəcəksiniz. Table seçimlərini sizə həvalə edirəm.
+
+Homework 1:
+Ən az 10 fərqli table'dan məlumat select edin.
+
+Homework 2:
+Ən az 5 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 1 column'a şərt qoyun.
+
+Homework 3:
+Ən az 5 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 2 column'a şərt qoyun (and ilə).
+
+Homework 4:
+Ən az 5 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 2 column'a şərt qoyun (or ilə).
+	
+Homework 5:
+Ən az 5 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 4 column'a şərt qoyun (and ilə).
+
+Homework 6:
+Ən az 5 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 4 column'a şərt qoyun (or ilə).
+
+Homework 7:
+Ən az 6 fərqli table'dan məlumat select edin. Hər select sorğusunda where'da 3-4 column'a şərt qoyun (and və or ilə).
+
+Homework 8:
+5 fərqli table'dan ilk 5, 10, 15, 20, 25 məlumatı çəkməyə çalışın. Birindən ilk 5, digərindən ilk 10 və s.
+
+Homework 9:
+Sales.SalesOrderDetail table'dan CarrierTrackingNumber'ində 480 rəqəmi keçən, OrderQty'i 15 olmayanları gətirin.
+	
+Homework 10:
+Sales.SalesOrderDetail table'dan UnitPrice'ı 250-dən böyük və ya bərabər olan amma 125 olmayan,
+ProductID'si 1-dən böyük və 1000-dən kiçik və ya bərabər olanları gətirin
+*/
+
+
+
+
+--------Asagidaki ev ve praktiki tapsiriqlar queries faylinda ayrilib diqqetli olun--------
+--------Asagidaki ev ve praktiki tapsiriqlar queries faylinda ayrilib diqqetli olun--------
+--------Asagidaki ev ve praktiki tapsiriqlar queries faylinda ayrilib diqqetli olun--------
+
+
+
+/*
+(IN və NOT IN keçəndən sonrakı hissələrin praktiki və ev tapşırıqları)
+Part 3:
+1. In istifadə edərək Person.Person table'ından FirstName column'u Scott, Frank, Eric olan məlumatların bütün column'larını gətir.
+2. In istifadə edərək Person.Person table'ından FirstName column'u Scott, Frank, Eric olan məlumatların FirstName, MiddleName, LastName column'larını FullName adı olaraq gətir.
+3. Not In istifadə edərək Person.Person table'ından FirstName column'u Eugene, Barry, Jack olmayan məlumatların bütün column'larını gətir.
+4. Not In istifadə edərək Person.Person table'ından FirstName column'u Eugene, Barry, Jack olmayan məlumatların BusinessEntityID column'unun toplamını Total adı olaraq gətir.
+5. Person.Address table'ından bütün dataları gətir. Ancaq PostalCode column'una görə A-dan Z'yə (artan) sırala.
+6. Person.Address table'ından bütün dataları gətir. Ancaq City column'una görə Z-dən A'ya (azalan) sırala.
+7. Person.Address table'ından City column'una görə qruplaşdıraraq neçə adres olduğunu çıxardın. Yəni hər şəhər üçün neçə adres məlumatı var? Eyni zamanda da şəhər sayına görə azalan sıralama edin.
+*/
---
+++ May/02/queries.sql
@@ -0,0 +1,213 @@
+﻿select * from Person.Person;
+
+select BusinessEntityID, FirstName, MiddleName, LastName from Person.Person;
+
+select BusinessEntityID, FirstName + ' ' + MiddleName + ' ' + LastName from Person.Person;
+
+select BusinessEntityID Sira, FirstName Ad, LastName Soyad from Person.Person;
+
+select BusinessEntityID, FirstName + ' ' + MiddleName + ' ' + LastName Fullname from Person.Person;
+
+
+
+select * from Person.Address;
+
+select top 20 * from Person.Address;
+
+select top 20 percent * from Person.Address;
+
+select * from Person.Person where FirstName = 'John';
+select * from Person.Person where BusinessEntityID = 9;
+select * from Person.Person where EmailPromotion = 1;
+
+/*
+Task In Lesson 1:
+1. Person.EmailAddress table'ından EmailAddressID'si 3 olan məlumatların bütün column'larını gətir.
+2. Production.ProductDocument table'ından ProductID'si 517 olan məlumatların bütün column'larını gətir.
+3. Sales.Currency table'ından CurrencyCode'u 'AZM' olan məlumatların bütün column'larını gətir.
+4. Sales.SalesTerritory table'ından TerritoryID'si 6 olan məlumatların sadəcə Name və CountryRegionCode column'larını gətir.
+*/
+
+select * from Person.EmailAddress where EmailAddressID = 3 --1
+
+select * from Production.ProductDocument where ProductID = 517 --2
+
+select * from Sales.Currency where CurrencyCode = 'AZM' --3
+
+select Name, CountryRegionCode from Sales.SalesTerritory where TerritoryID=6; --4
+
+
+select [group] from Sales.SalesTerritory where TerritoryID=6;
+
+
+
+
+--[Multiple where clause]
+--and
+select * from Person.Person where FirstName = 'John' and LastName = 'Arthur';
+select * from Person.Person where PersonType = 'EM' and BusinessEntityID = 155;
+
+--or
+select * from Person.Person where FirstName = 'John' or FirstName = 'Rob';
+select * from Person.Person where FirstName = 'Orkhan' or FirstName = 'Rob';
+
+--and - or
+select * from Person.Person where FirstName = 'John' or FirstName = 'Rob' and PersonType='IN';
+select * from Person.Person where (FirstName = 'John' or FirstName = 'Rob') and PersonType='IN';
+
+select * from Person.Person where FirstName = 'John' or FirstName = 'Rob'
+
+
+
+--[Comparsion Operators]
+select * from Production.Product;
+-- > boyukdur
+select * from Production.Product where ListPrice > 500
+select * from Production.Product where ListPrice > 200
+
+-- < kicikdir
+select * from Production.Product where ListPrice < 650
+select * from Production.Product where ListPrice < 50
+
+-- >= boyukdur beraberdir
+select * from Production.Product where ListPrice >= 500
+select * from Production.Product where ListPrice >= 200
+
+-- <= kicikdir beraberdir
+select * from Production.Product where ListPrice <= 120
+select * from Production.Product where ListPrice <= 50
+
+-- beraberdir
+select * from Production.Product where Color = 'Black'
+select * from Production.Product where Color = 'Yellow'
+
+-- deyilse beraber
+select * from Production.Product where Color != 'Black'
+select * from Production.Product where Color != 'Yellow'
+
+
+--[Like Operator]
+-- a herfile baslayan
+--like 'a%'
+
+-- a herfile biten
+--like '%a'
+
+-- icinde a herfi olan
+--like '%a%'
+
+select * from Person.Person where FirstName like 'a%';
+select * from Person.Person where FirstName like '%a';
+select * from Person.Person where FirstName like '%a%';
+
+select * from Person.Person where FirstName like '%al';
+select * from Person.Person where FirstName like '%son%';
+
+
+--------Asagidaki kodlarin ev ve praktiki tapsiriqlari homework faylinda da ayrilib diqqetli olun--------
+--------Asagidaki kodlarin ev ve praktiki tapsiriqlari homework faylinda da ayrilib diqqetli olun--------
+--------Asagidaki kodlarin ev ve praktiki tapsiriqlari homework faylinda da ayrilib diqqetli olun--------
+
+
+
+--[IN - NOT IN]
+select * from Production.Product;
+
+select * from Production.Product where
+ProductNumber = 'AR-5381' or
+ProductNumber = 'BA-8327' or
+ProductNumber = 'BE-2349' or
+ProductNumber = 'BE-2908' or
+ProductNumber = 'BL-2036';
+
+select * from Production.Product where
+ProductNumber
+IN ('AR-5381', 'BA-8327', 'BE-2349', 'BE-2908', 'BL-2036')
+
+select * from Production.Product where
+ProductNumber != 'AR-5381' and
+ProductNumber != 'BA-8327' and
+ProductNumber != 'BE-2349' and
+ProductNumber != 'BE-2908' and
+ProductNumber != 'BL-2036';
+
+select * from Production.Product where
+ProductNumber
+NOT IN ('AR-5381', 'BA-8327', 'BE-2349', 'BE-2908', 'BL-2036')
+
+--[Order By (sorting)]
+select * from Production.Product where Name like '%a%'
+select * from Production.Product where Name like '%a%' order by ProductID
+
+select * from Production.Product where Name like '%a%' order by Name
+
+select * from Production.Product where Name like '%a%' order by Name desc
+
+select * from Production.Product where Name like '%a%' order by ProductID desc
+
+
+select * from Sales.Store order by SalesPersonID
+select * from Sales.Store order by SalesPersonID asc
+
+select * from Sales.Store order by SalesPersonID desc
+
+
+select * from Sales.Store order by Name asc
+select * from Sales.Store order by Name desc
+
+--A-Z siralama: asc (ascending)
+--Z-A siralama: desc (descending)
+--1-N
+--N-1
+
+
+
+
+--[Group By]
+select * from Production.Product
+
+select * from Production.Product where Color is not null
+
+select Color, SUM(SafetyStockLevel) StockLevel, AVG(ListPrice) AvgPrice from Production.Product
+where Color is not null
+group by Color
+
+select Color, SUM(SafetyStockLevel) StockLevel, AVG(ListPrice) AvgPrice from Production.Product
+group by Color
+
+
+
+--[Distinct]
+select * from Production.Product
+select distinct Color from Production.Product
+select distinct Color, Name from Production.Product
+
+
+
+select * from Sales.SalesOrderDetail
+select distinct ProductID from Sales.SalesOrderDetail
+
+select * from Production.Product
+where
+ProductID IN
+(
+	select distinct ProductID from Sales.SalesOrderDetail
+)
+
+
+select Color, sum(ListPrice) from Production.Product
+where
+ProductID IN
+(
+	select distinct ProductID from Sales.SalesOrderDetail
+)
+group by Color
+
+
+
+--[Between]
+select * from Production.Product
+
+select * from Production.Product where ListPrice >= 50 and ListPrice <= 100
+
+select * from Production.Product where ListPrice between 50 and 100
