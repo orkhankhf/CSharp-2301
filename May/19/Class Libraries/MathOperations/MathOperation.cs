607919108bc974c9ba4e132cc32067eb4f7cdfad
Orkhan Farajov <orkhankhf@gmail.com>
2023-05-20 00:24:58 +0400

    Libraries and teorical information of API

---
+++ May/19/Class Libraries/MathOperations/MathOperation.cs
@@ -0,0 +1,56 @@
+﻿using MathOperations.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public static class MathOperation
+    {
+        #region Public methods
+        public static decimal Calculate(decimal a, decimal b, MathOperationTypeEnum operationType)
+        {
+            switch (operationType)
+            {
+                case MathOperationTypeEnum.Addition:
+                    return Addition(a, b);
+                case MathOperationTypeEnum.Subtraction:
+                    return Subtraction(a, b);
+                case MathOperationTypeEnum.Multiplication:
+                    return Multiplication(a, b);
+                case MathOperationTypeEnum.Divide:
+                    return Divide(a, b);
+                default:
+                    throw new Exception("Səhv əməliyyat növü seçilib!");
+            }
+        }
+        #endregion
+
+        #region Private methods
+        private static decimal Addition(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        private static decimal Subtraction(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        private static decimal Multiplication(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        private static decimal Divide(decimal a, decimal b)
+        {
+            if (b == 0)
+                throw new Exception("Ədədi sıfıra bölə bilməzsiniz!");
+
+            return a / b;
+        }
+        #endregion
+    }
+}
---
+++ May/19/SomeProject/SomeProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using MathOperations;
+using MathOperations.Enums;
+
+namespace SomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var additionRes = MathOperation.Calculate(15, 20, MathOperationTypeEnum.Addition);
+            var subtractionRes = MathOperation.Calculate(100, 35, MathOperationTypeEnum.Subtraction);
+            var multiplicationRes = MathOperation.Calculate(5, 40, MathOperationTypeEnum.Multiplication);
+            var divideRes = MathOperation.Calculate(30, 0, MathOperationTypeEnum.Divide);
+        }
+    }
+}
