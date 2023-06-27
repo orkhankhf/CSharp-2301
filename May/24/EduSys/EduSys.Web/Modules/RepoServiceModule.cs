2726263234d9e7a580776d8421e38720766a3ace
Orkhan Farajov <orkhankhf@gmail.com>
2023-06-28 00:31:45 +0400

    Creating ProductsController in MVC side

---
+++ May/24/EduSys/EduSys.Web/Modules/RepoServiceModule.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using EduSys.Core.Repositories;
+using EduSys.Core.Services;
+using EduSys.Core.UnitOfWorks;
+using EduSys.Repository.Repositories;
+using EduSys.Repository.UnitOfWorks;
+using EduSys.Repository;
+using EduSys.Service.Mapping;
+using EduSys.Service.Services;
+using System.Reflection;
+
+namespace EduSys.Web.Modules
+{
+    public class RepoServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var apiAssembly = Assembly.GetExecutingAssembly();
+
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+        }
+    }
+}
--- May/24/EduSys/EduSys.Web/Program.cs
+++ May/24/EduSys/EduSys.Web/Program.cs
@@ -1,7 +1,31 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using EduSys.Repository;
+using EduSys.Service.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using EduSys.Web.Modules;
+using FluentValidation.AspNetCore;
+using EduSys.Service.Validations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 var app = builder.Build();
 
